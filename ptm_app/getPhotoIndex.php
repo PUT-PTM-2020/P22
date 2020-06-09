cfc2726701349bb236a47989ea2c271fe6c78e32
saimon0 <57774469+saimon0@users.noreply.github.com>
2020-06-09 11:51:27 +0200

    Add files via upload

---
+++ ptm_app/getPhotoIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+$return_array = array();
+
+$photoIndex = file_get_contents("photoIndex.txt");
+
+echo $photoIndex;
+
+#$return_array[] = array(
+#  "photoIndex" => $photoIndex,
+#);
+
+#echo json_encode($return_array);
